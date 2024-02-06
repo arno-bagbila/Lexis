b015be5e8103499297e62c38fb3aedf0783a392b
Arnaud Bagbila <beat4free@gmail.com>
2024-02-06 16:43:28 +0000

    adding data cleaner project

---
+++ Lexis.DataCleaner/CleanerService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MongoDB.Driver;
+
+public class CleanerService : IHostedService
+{
+    private readonly ILogger _logger;
+    private readonly IMongoClient _client;
+    private readonly IConfiguration _configuration;
+    
+    public CleanerService(ILogger<CleanerService> logger, IMongoClient client, IConfiguration configuration)
+    {
+        _logger = logger;
+        _client = client;
+        _configuration = configuration;
+    }
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        var database = _client.GetDatabase(_configuration.GetValue<string>("ConnectionStrings:DatabaseName"));
+        var blogCollection = database.GetCollection<Domain.Entities.Blog>(nameof(Domain.Entities.Blog));
+        var filter = Builders<Domain.Entities.Blog>.Filter.Lt(x => x.PublishedOn, DateTime.Now.AddHours(-1));
+        var result = blogCollection.DeleteMany(filter);
+        _logger.LogInformation($"Deleted {result.DeletedCount} documents");
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ Lexis.DataCleaner/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MongoDB.Driver;
+using Serilog;
+
+internal sealed class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = Host.CreateApplicationBuilder(args);
+        builder.Services.AddHostedService<CleanerService>();
+        
+        var configurationFile = Path.Combine("Configuration", "appsettings.json");
+        var environmentConfigurationFile = Path.Combine("Configuration", $"appsettings.{builder.Environment.EnvironmentName}.json");
+        
+            builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile(configurationFile, optional: false, reloadOnChange: true)
+            .AddJsonFile(environmentConfigurationFile, optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+        
+        var connStr = builder.Configuration.GetConnectionString("MongoDb");
+        builder.Services.AddSingleton<IMongoClient, MongoClient>(sp => new MongoClient(connStr));
+        
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(builder.Configuration)
+            .CreateLogger();
+        builder.Logging.AddSerilog(Log.Logger);
+        
+        var host = builder.Build();
+        host.Run();
+    }
+}
